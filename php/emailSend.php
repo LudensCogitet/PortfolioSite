ecba138f0255752d43e94bd1e687080f355c5f73
John Hiner <jrwenceslaus@gmail.com>
2017-04-27 15:36:08 -0400

    e-mail form working, what?!

--- php/emailSend.php
+++ php/emailSend.php
@@ -1,12 +1,25 @@
 <?php
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
-	$replyTo = strip_tags($_POST['email']);
-	$subject = strip_tags($_POST['subject']);
-	$text = wordwrap(strip_tags($_POST['text']),50,"\r\n");
-	$headers = 'From: contactForm@johnhiner.com' . "\r\n".
+	$replyTo = strip_tags($_POST['replyTo']);
+	$replyTo = filter_var($replyTo,FILTER_SANITIZE_EMAIL);
+	
+	if(!filter_var($replyTo,FILTER_VALIDATE_EMAIL)){
+		echo "bad email";
+		return;
+	}
+	else{
+		$subject = strip_tags($_POST['subject']);
+		$text = wordwrap(strip_tags($_POST['text']),50,"\r\n");
+		$headers = 'From: contactForm@johnhiner.com' . "\r\n".
 						 'Reply-To: '.$replyTo. "\r\n".
 						 'X-Mailer: PHP/'.phpversion();
 	
-	mail("johnhineriii@protonmail.com",$subject,$text,$headers);
+		if(!mail("johnhineriii@protonmail.com",$subject,$text,$headers)){
+			echo "failure";
+		}
+		else{
+			echo "success";
+		}
+	}
 }
 ?>
