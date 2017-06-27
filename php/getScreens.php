b5344f151d3629641147b0cdc807a75fd75b1e14
John Hiner <Jrwenceslaus@gmail.com>
2017-06-27 14:02:47 -0400

    changed the look a bit

--- php/getScreens.php
+++ php/getScreens.php
@@ -1,14 +1,14 @@
 <?php
 	if($_SERVER['REQUEST_METHOD'] == 'POST'){
 		$screens = json_decode(file_get_contents('../data/screens.json'))->screens;
-		
+
 		$returnObj = ["menu" => "",
 									"screens" => 				[]];
-		
+
 		$numScreens = count($screens);
-		
+
 		$d = -1;
-		
+
 		for($i = 0; $i < $numScreens; $i++){
 			if(array_key_exists("START",$screens[$i])){
 				$returnObj["menu"] .= "<li class='category'>".$screens[$i]->START."<span class='arrow right'><img src='data/img/rightArrow.png'></span><span class='arrow down' style='display: none;'><img src='data/img/downArrow.png'></span><ul class='navMenu submenu'>";
@@ -21,7 +21,7 @@
 				$entryText = $screens[$i]->text;
 				$entryText = file_get_contents($entryText);
 				$screens[$i]->text = $entryText;
-				
+
 				$newScreen;
 				$newContent;
 				$newHeading;
@@ -31,9 +31,9 @@
 				$newImage;
 				$sourceCodeLink = "";
 				$liveDemoLink = "";
-				
+
 				$close = '</div>';
-				
+
 				$classes = "screen";
 				if($i == 0){
 					$classes .= " firstScreen";
@@ -51,30 +51,30 @@
 					$newNextButton = "<div class='button nextButton' data-target=".($d+1).">&#8681;</div>";
 					$returnObj["menu"] .= "<li class='option' data-target=".$d.">".$screens[$i]->name."</li>";
 				}
-				
+
 				if(array_key_exists('sourceCodeLink',$screens[$i])){
 					$sourceCodeLink = "<div class='infoLink'><a title='See the source on GitHub' target='_blank' href='".$screens[$i]->sourceCodeLink."'><div><img src='data/img/GitHub-Mark-64px.png'><div>GitHub</div></div></a></div>";
 				}
-				
+
 				if(array_key_exists('liveDemoLink',$screens[$i])){
 					$liveDemoLink = "<div class='infoLink'><a title='Check out the app in action' target='_blank' href='".$screens[$i]->liveDemoLink."'><div><img src='data/img/bigPlay.png'><div>Demo</div></div></a></div>";
 				}
-				
+
 				$newScreen = "<div id=".$d." class='".$classes."'>".
 												$newPrevButton.
 												"<div class='content light'>".
 													"<div class='heading'>".
-														"<div style='display: inline-block; float: left; width: 60%; margin: 0;'>".
-															"<h1>".$screens[$i]->heading."</h1>".
-															"<h3 class='darker'>".$screens[$i]->subheading."</h3>".
-														"</div>".
-														"<div style='display: inline-block; float: right; width: 40%; margin: 0;'>".
+														//"<div style='display: inline-block; float: left; width: 60%; margin: 0;'>".
+														"<div style='display: inline-block; float:right; margin: 0;'>".
 															$sourceCodeLink.
 															$liveDemoLink.
 														"</div>".
-													"</div>".	
+															"<h1>".$screens[$i]->heading."</h1>".
+															"<h3 class='darker'>".$screens[$i]->subheading."</h3>".
+														//"</div>".
+													"</div>".
 													"<div class='text'>".
-														"<span><img class='pic' src='".$screens[$i]->image."'></span>".
+														"<img class='pic' src='".$screens[$i]->image."'>".
 														$screens[$i]->text.
 														"<div style='clear: both;'></div>".
 													"</div>".
@@ -84,7 +84,7 @@
 				$returnObj["screens"][] = $newScreen;
 			}
 		}
-		
+
 		echo json_encode($returnObj,JSON_UNESCAPED_SLASHES);
 	}
 ?>
